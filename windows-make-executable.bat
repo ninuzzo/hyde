6edd2b4e54fc77c23488c471e13e14779ba2d412
Antonio Bonifati <antonio.bonifati@gmail.com>
2012-08-03 20:09:51 +0200

    Fixed build script for Windows

--- windows-make-executable.bat
+++ windows-make-executable.bat
@@ -6,6 +6,7 @@ call "C:\Program Files\Microsoft Visual Studio 10.0\VC\bin\vcvars32.bat"
 
 "%ECL%\ecl" -norc -shell make-executable.lisp
 
-echo Now copy to hyde-server.exe
-echo Its dependencies are ecl.dll sockets.fas ucd.dat
+echo Now copy that to hyde-server.exe
+echo Its dependencies are: ecl.dll sockets.fas and must be re-copied
+echo from the ECL install dir after each ECL update
 pause
